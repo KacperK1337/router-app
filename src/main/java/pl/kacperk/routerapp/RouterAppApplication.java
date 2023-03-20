2a76f03b0ce55b966d0711235ac3a80b8fdb7b25
Kacper Koźmiński <kacper.kozminski1337@proton.me>
2023-03-20 13:21:55 +0100

    Initial commit

---
+++ src/main/java/pl/kacperk/routerapp/RouterAppApplication.java
@@ -0,0 +1,13 @@
+package pl.kacperk.routerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kacperk/routerapp/RouterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kacperk.routerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
